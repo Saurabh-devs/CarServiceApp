036faa2816b1c6dc794ac3b0b72cd44d79f6cb60
SSR12345678 <skfzd2003@gmail.com>
2025-09-28 00:13:30 +0530

    Commit "Customer File Updated"

--- src/Customer.java
+++ src/Customer.java
@@ -1,26 +1,26 @@
-// public class Customer {
+public class Customer {
 
-//     private String name;
-//     private String phone;
-//     private  Car car;
+    private String name;
+    private String phone;
+    private  Car car;
 
-//     public Customer(String name, String phone, Car car) {
-//         this.name = name;
-//         this.phone = phone;
-//         this.car = car;
-//     }
+    public Customer(String name, String phone, Car car) {
+        this.name = name;
+        this.phone = phone;
+        this.car = car;
+    }
 
-//     public Car getCar() {
-//         return car;
-//     }
+    public Car getCar() {
+        return car;
+    }
 
-//     public String getPhone() {
-//         return phone;
-//     }
+    public String getPhone() {
+        return phone;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public String getName() {
+        return name;
+    }
 
 
-// }
+}
