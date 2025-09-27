0c08d3617fd81c154a637df856e54c1e2926e1e4
SSR12345678 <skfzd2003@gmail.com>
2025-09-28 00:12:37 +0530

    commit "car file updated"

--- src/Car.java
+++ src/Car.java
@@ -1,26 +1,26 @@
-// public class Car {
+public class Car {
 
-//     private String carNumber;
-//     private String model;
+    private String carNumber;
+    private String model;
 
-//     public Car(String carNumber, String model) {
-//         this.carNumber = carNumber;
-//         this.model = model;
-//     }
+    public Car(String carNumber, String model) {
+        this.carNumber = carNumber;
+        this.model = model;
+    }
 
-//     public String getCarNumber() {
-//         return carNumber;
-//     }
+    public String getCarNumber() {
+        return carNumber;
+    }
 
-// //    public void setCarNumber(String carNumber) {
-// //        this.carNumber = carNumber;
-// //    }
+//    public void setCarNumber(String carNumber) {
+//        this.carNumber = carNumber;
+//    }
 
-//     public String getModel() {
-//         return model;
-//     }
+    public String getModel() {
+        return model;
+    }
 
-// //    public void setModel(String model) {
-// //        this.model = model;
-// //    }
-// }
+//    public void setModel(String model) {
+//        this.model = model;
+//    }
+}
